e66bc9c383c0195e20f428b2eb118060e7222780
draganstojanov <dragan.stojanov@gmail.com>
2019-03-03 23:43:39 +0100

    20190303-START

---
+++ app/src/main/java/com/andraganoid/verymuchtodo/Model/TodoItem.java
@@ -0,0 +1,16 @@
+package com.andraganoid.verymuchtodo.Model;
+
+public class TodoItem {
+
+    private String content;
+    //    private String quantity;
+//    private String description;
+    private long createdTimestamp;
+    private String createdUser;
+    private long lastChangeTimestamp;
+    private String lastChangeTUser;
+    private long executedTimestamp;
+    private String executedUser;
+    private boolean done;
+
+}
---
+++ app/src/main/java/com/andraganoid/verymuchtodo/Model/User.java
@@ -0,0 +1,17 @@
+package com.andraganoid.verymuchtodo.Model;
+
+import android.location.Location;
+
+public class User {
+    private String id;
+    private String name;
+    private String lastName;
+    private String screenName;
+    private boolean online;
+    private Location location;
+    //  private long locationTimestamp;
+
+    public User() {
+        // location.setTime();
+    }
+}
