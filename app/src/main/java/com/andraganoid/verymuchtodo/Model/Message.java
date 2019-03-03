132e323c2cdd059c4a5c3a86ced32ee0eb8e6d70
draganstojanov <dragan.stojanov@gmail.com>
2019-03-04 00:35:20 +0100

    20190303-START-2

---
+++ app/src/main/java/com/andraganoid/verymuchtodo/Model/Message.java
@@ -0,0 +1,10 @@
+package com.andraganoid.verymuchtodo.Model;
+
+public class Message {
+
+    private String text;
+    private long timestamp;
+    private boolean setForDeletion;
+    private String from;
+
+}
---
+++ app/src/main/java/com/andraganoid/verymuchtodo/Model/TodoList.java
@@ -0,0 +1,14 @@
+package com.andraganoid.verymuchtodo.Model;
+
+public class TodoList {
+
+    private String name;
+    private String shortDescription;
+    private long createdTimestamp;
+    private long lastChangedTimestamp;
+    private String createdByUser;
+    private String lastChangedByUser;
+    private int category;
+    private boolean allDone;
+    private boolean setForDeletion;
+}
