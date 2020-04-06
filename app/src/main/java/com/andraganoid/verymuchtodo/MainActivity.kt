91c115cd37c01daeb6df749141ca702142ba7d7c
draganstojanov <dragan.stojanov@gmail.com>
2020-04-06 08:50:11 +0200

    firebase ui auth

--- app/src/main/java/com/andraganoid/verymuchtodo/MainActivity.kt
+++ app/src/main/java/com/andraganoid/verymuchtodo/MainActivity.kt
@@ -12,3 +12,11 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = ContextCompat.getColor(this, R.color.backgorundColor)
     }
 }
+
+
+//Store: C:\Users\Stojanov 4\.android\debug.keystore
+//Alias: AndroidDebugKey
+//MD5: 7D:A1:4F:55:01:77:DF:DA:4A:89:8C:1A:D1:A0:77:70
+//SHA1: F4:35:B9:4F:4C:96:5E:2D:23:8A:CB:53:39:F0:AA:6D:49:53:CF:F1
+//SHA-256: 06:E8:B5:62:56:3C:AF:DA:AA:55:56:75:73:AB:E6:24:36:1C:8D:F6:43:70:BF:9A:C9:46:82:4F:4D:E3:EA:48
+//Valid until: Tuesday, March 8, 2050
--- app/src/main/java/com/andraganoid/verymuchtodo/kauth/LoginFragment.kt
+++ app/src/main/java/com/andraganoid/verymuchtodo/kauth/LoginFragment.kt
@@ -1,19 +1,69 @@
 package com.andraganoid.verymuchtodo.kauth
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.andraganoid.verymuchtodo.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
 
    // private lateinit var viewModel: LoginViewModel
 
+    private val RC_SIGN_IN = 1234;
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.PhoneBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+               AuthUI.IdpConfig.FacebookBuilder().build(),
+                AuthUI.IdpConfig.TwitterBuilder().build())
+
+// Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                // ...
+                Toast.makeText(activity,"SIGNED IN",Toast.LENGTH_LONG).show()
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                Toast.makeText(activity,"FAILED",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 
