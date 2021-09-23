18007a39ef3189f743a3487e7eebd761bbeea0aa
Dragan <dragan.stojanov@gmail.com>
2021-09-23 22:55:11 +0200

    done

--- app/src/main/java/com/andraganoid/verymuchtodo/main/MainActivity.kt
+++ app/src/main/java/com/andraganoid/verymuchtodo/main/MainActivity.kt
@@ -13,9 +13,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModel()
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
