45b000908839c054810be33212e5548fbdf00792
dragan.stojanov <dragan.stojanov@gmail.com>
2023-04-08 13:42:15 +0200

    prepare for compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     kotlin("kapt")
     id("com.google.firebase.crashlytics")
     id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
+@Suppress("UnstableApiUsage")
 android {
     namespace = "com.andraganoid.verymuchtodo"
     compileSdk = 33
@@ -61,34 +63,77 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
+
+    packagingOptions {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+
+
 }
 
 
 dependencies {
 
+    val composeVersion = "1.4.0"
     val lifecycleVersion = "2.6.1"
+    val coroutinesVersion = "1.6.4"
+    val materialVersion = "1.4.1"
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.activity:activity-compose:1.7.0")
+   // implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.material:material:$materialVersion")
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
 
     // Kotlin
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
-    //Gson
-    implementation("com.google.code.gson:gson:2.10.1")
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
-    // Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-
-    // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    //Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
 
     // Koin
     implementation("io.insert-koin:koin-android:3.4.0")
+
+
+    implementation("androidx.appcompat:appcompat:1.6.1")//TODO REMOVE AT END
+    implementation("com.google.android.material:material:1.8.0")//TODO REMOVE AT END
+
+
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")//TODO REMOVE AT END
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")//TODO REMOVE AT END
+
+    // Lifecycle
+ //   implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+ //   implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")//TODO REMOVE AT END
+ //   implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+
+
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:31.4.0"))
@@ -98,7 +143,17 @@ dependencies {
     implementation("com.google.firebase:firebase-crashlytics-ktx")
 
     // Timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")//TODO REMOVE AT END
 
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
 
+}
+
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,8 @@
-buildscript {
 
-    repositories {
-        google()
-        mavenCentral()
-
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.4")
-        classpath("com.google.gms:google-services:4.3.15")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+plugins {
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.firebase.crashlytics") version "2.9.4" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
